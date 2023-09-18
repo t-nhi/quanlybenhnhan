2704abf5126fe541d8f218c59617cceb95e71bd3
Nhi Vũ Thị Tuyết <102220077@sv1.dut.udn.vn>
2023-09-18 15:43:45 +0700

    add thong tin benh nhan

---
+++ quanlybenhnha/fThongtin.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace quanlybenhnha
+{
+    partial class fThongtin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            panel3 = new Panel();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(panel2);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1089, 573);
+            panel1.TabIndex = 0;
+            panel1.Paint += panel1_Paint;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(label13);
+            panel2.Controls.Add(label6);
+            panel2.Controls.Add(label5);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(label1);
+            panel2.Location = new Point(18, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1051, 272);
+            panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(26, 50);
+            label1.Name = "label1";
+            label1.Size = new Size(113, 24);
+            label1.TabIndex = 0;
+            label1.Text = "Họ và Tên:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(609, 98);
+            label2.Name = "label2";
+            label2.Size = new Size(141, 24);
+            label2.TabIndex = 1;
+            label2.Text = "Số điện thoại:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(26, 98);
+            label3.Name = "label3";
+            label3.Size = new Size(73, 24);
+            label3.TabIndex = 2;
+            label3.Text = "CCCD:";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(26, 193);
+            label4.Name = "label4";
+            label4.Size = new Size(81, 24);
+            label4.TabIndex = 3;
+            label4.Text = "Địa chỉ:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(609, 50);
+            label5.Name = "label5";
+            label5.Size = new Size(100, 24);
+            label5.TabIndex = 4;
+            label5.Text = "Giới tính:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Arial", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(3, 3);
+            label6.Name = "label6";
+            label6.Size = new Size(230, 27);
+            label6.TabIndex = 5;
+            label6.Text = "Thông Tin Cá Nhân";
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(label7);
+            panel3.Controls.Add(label8);
+            panel3.Controls.Add(label9);
+            panel3.Controls.Add(label10);
+            panel3.Controls.Add(label11);
+            panel3.Controls.Add(label12);
+            panel3.Location = new Point(18, 299);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(1051, 242);
+            panel3.TabIndex = 1;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Arial", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(3, 2);
+            label7.Name = "label7";
+            label7.Size = new Size(194, 27);
+            label7.TabIndex = 5;
+            label7.Text = "Thông Tin Bệnh";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.Location = new Point(439, 47);
+            label8.Name = "label8";
+            label8.Size = new Size(121, 24);
+            label8.TabIndex = 4;
+            label8.Text = "Chẩn Đoán:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label9.Location = new Point(26, 139);
+            label9.Name = "label9";
+            label9.Size = new Size(132, 24);
+            label9.TabIndex = 3;
+            label9.Text = "Triệu chứng:";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label10.Location = new Point(26, 95);
+            label10.Name = "label10";
+            label10.Size = new Size(147, 24);
+            label10.TabIndex = 2;
+            label10.Text = "Bác sĩ điều trị:";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label11.Location = new Point(440, 95);
+            label11.Name = "label11";
+            label11.Size = new Size(137, 24);
+            label11.TabIndex = 1;
+            label11.Text = "Y tá theo dõi:";
+            label11.Click += label11_Click;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label12.Location = new Point(26, 47);
+            label12.Name = "label12";
+            label12.Size = new Size(90, 24);
+            label12.TabIndex = 0;
+            label12.Text = "Giường:";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label13.Location = new Point(26, 146);
+            label13.Name = "label13";
+            label13.Size = new Size(103, 24);
+            label13.TabIndex = 6;
+            label13.Text = "Mã BHYT:";
+            // 
+            // fThongtin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1113, 597);
+            Controls.Add(panel1);
+            Name = "fThongtin";
+            Text = "Thông tin bệnh nhân";
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Panel panel3;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label6;
+        private Label label13;
+    }
+}
